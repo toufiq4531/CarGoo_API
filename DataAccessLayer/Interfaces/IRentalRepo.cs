e03e9aa183a74e38bbfa6f6158d6a52989ee9102
Mohammad Tofiqul Islam <pranto.toufiq001@gmail.com>
2025-10-06 05:07:22 +0600

    Repos and Interfaces

---
+++ DataAccessLayer/Interfaces/IRentalRepo.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface IRentalRepo
+    {
+        bool Create(Rental r);
+        List<Rental> Get();
+        Rental Get(int id);
+        bool Update(Rental r);
+        bool Delete(int id);
+    }
+}
---
+++ DataAccessLayer/Interfaces/IVehicleRepo.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.EF.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    internal interface IVehicleRepo
+    {
+        bool Create(Vehicle v);
+        List<Vehicle> Get();
+        Vehicle Get(int id);
+        bool Update(Vehicle v);
+        bool Delete(int id);
+    }
+}
