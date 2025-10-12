3953b2613ddfdef5140d01ccc518448ed0144729
Mohammad Tofiqul Islam <pranto.toufiq001@gmail.com>
2025-10-12 12:20:13 +0600

    Final update

---
+++ BusinessLogicLayer/DTOs/EstimateRentalCostDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DTOs
+{
+    public class EstimateRentalCostDTO
+    {
+        public int Days { get; set; }
+        public double EstimatedCost { get; set; }
+        public double DiscountedAmount { get; set; }
+        public double FinalCost { get; set; }
+    }
+}
